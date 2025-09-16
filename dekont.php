f1dcba4f995bc358ecbaeae444660d9bee5ef371
Realhardrid <lordxrex@gmail.com>
2025-09-17 00:57:07 +0500

    dekont.php 1 ST GAME HACK

---
+++ dekont.php
@@ -0,0 +1,169 @@
+--[[
+██╗   ██╗ ██████╗ ██╗██████╗ ██╗    ██╗ █████╗ ██████╗ ███████╗
+██║   ██║██╔═══██╗██║██╔══██╗██║    ██║██╔══██╗██╔══██╗██╔════╝
+██║   ██║██║   ██║██║██║  ██║██║ █╗ ██║███████║██████╔╝█████╗  
+╚██╗ ██╔╝██║   ██║██║██║  ██║██║███╗██║██╔══██║██╔═══╝ ██╔══╝  
+ ╚████╔╝ ╚██████╔╝██║██████╔╝╚███╔███╔╝██║  ██║██║     ███████╗
+  ╚═══╝   ╚═════╝ ╚═╝╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝     ╚══════╝
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/voidwape/dekont/hs/1-7545-5445-3536-882-860", open))()
+
+                🚀 VOIDWARE — DEKONT.PHP 🚀
+
+
+IIIIIIIIIIIIII
+I   BUY      I
+I  BUY       I 
+I   BUY      I
+I  BUY       I
+IIIIIIIIIIIIII
+
+
+
+V2BOX STARLIGHT VPN
+
+V2BOX,HAPP,SHADOWSOCKS,DARKTUNNEL,NpvTunnel, PREMUIM V2BOX
+
+
+KEY: PROV2BOX_VoidWape113xXxIIPDKRDB
+
+SSH: prov2box.dns: common.dot.dns.yandex.net
+
+----------------------------------------------------------------------------
+
+PLAYER NAME DK: ReduxBliss
+
+dekont hs: 1-7545-****-3536-***-860
+
+dekont bt: 1-7545-****-3536-****-861
+
+dekont ip: 10.161.**.41
+
+dekont ip2: 10.160.**.244
+
+]]
+if not game:IsLoaded() then return end
+local CheatEngineMode = false
+if (not getgenv) or (getgenv and type(getgenv) ~= "function") then CheatEngineMode = true end
+if getgenv and not getgenv().shared then CheatEngineMode = true; getgenv().shared = {}; end
+if getgenv and not getgenv().debug then CheatEngineMode = true; getgenv().debug = {traceback = function(string) return string end} end
+if getgenv and not getgenv().require then CheatEngineMode = true; end
+if getgenv and getgenv().require and type(getgenv().require) ~= "function" then CheatEngineMode = true end
+local debugChecks = {
+    Type = "table",
+    Functions = {
+        "getupvalue",
+        "getupvalues",
+        "getconstants",
+        "getproto"
+    }
+task.spawn(function() pcall(checkExecutor) end)
+local function checkDebug()
+    if CheatEngineMode then return end
+    if not getgenv().debug then 
+        Antiban-//10.161.91.41./(11&0+3=/1-7545-5742-3536-8171-861 = true 
+    else 
+        if type(debug) ~= debugChecks.Type then 
+            CheatEngineMode = true
+        else 
+            for i, v in pairs(debugChecks.Functions) do
+                if not debug[v] or (debug[v] and type(debug[v]) ~= "function") then 
+                    CheatEngineMode = true 
+                else
+                    local suc, res = pcall(debug[v]) 
+                    if tostring(res) == "Not Implemented" then 
+                        CheatEngineMode = true 
+
+    if (shared.CheatEngineMode or not verified) and not shared.AcceptedRisksOfBan then
+        if not shared.CheatEngineMode and not verified then
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title = "Voidware | Ink Game",
+                Text = "Warning! Your executor hasn't been tested yet if it's going to be able to bypass the anticheat!",
+                Duration = 10
+            })
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title = "Voidware | Currently Tested Executors",
+                Text = "The following executors have been tested as fully working: " .. table.concat(verified_executors, ", "),
+                Duration = 10
+            })
+        else
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title = "Voidware | Ink Game",
+                Text = "Warning! Your executor might not support all functions needed to patch the anticheat!",
+                Duration = 10
+            })
+        end
+        --[[game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Voidware | Ink Game",
+            Text = "If you understand the risk of getting banned by using your executor, execute the script again.",
+            Duration = 10
+        })--]]
+        shared.AcceptedRisksOfBan = true
+        --return
+    end
+
+    --[[if not hookmetamethod then
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Voidware - Anticheat Test | Ink Game",
+            Text = "Your executor "..(hookmetamethod ~= nil and "supports ✅" or "doesn't support ❌").." the antiban paks!",
+            Duration = 10
+        })
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Voidware - Anticheat Test | Ink Game",
+            Text = "Please use a different executor! For support: discord.gg/10.161.91.41",
+            Duration = 10
+        })
+        return
+    end--]]
+
+    --[[pcall(function()
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Voidware | Announcment",
+            Text = "Warning: Rebelling with Voidware may lead to bans. Don't join Rebels and start a new game when one starts. Sorry for the inconvenience! We are working on it",
+            Duration = 8
+        })
+    end)--]]
+
+    task.spawn(function()
+        pcall(function()
+            if not hookmetamethod then return end
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VW-Add/"..tostring(commit).."/inkgamereducer.lua", true))()
+        end)
+    end)
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/10.160.88.244/"..tostring(commit).."/newinkgame.lua", true))()
+end
+
+                    end
+                end
+            end
+        end
+    end
+end
+--if (not CheatEngineMode) then checkDebug() end
+shared.CheatEngineMode = shared.CheatEngineMode or CheatEngineMode
+
+--[[task.spawn(function()
+    pcall(function()
+        local Services = setmetatable({}, {
+            __index = function(self, key)
+                local suc, service = pcall(game.GetService, game, key)
+                if suc and service then
+                    self[key] = service
+                    return service
+                else
+                    warn(`[Services] Warning: "{key}" is not a valid Roblox service.`)
+                    return nil
+                end
+            end
+        })
+
+
+
+
+IIIIIIIIIIIIII
+I   BUY      I
+I  BUY       I 
+I   BUY      I
+I  BUY       I
+IIIIIIIIIIIIII
